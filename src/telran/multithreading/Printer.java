71e48cbd6ae2bf3cadd8a184851dc586b9203818
AlexKovalchuk <kovalchuk111@gmail.com>
2023-09-11 01:54:15 +0300

    hw-44

--- src/telran/multithreading/Printer.java
+++ src/telran/multithreading/Printer.java
@@ -2,21 +2,48 @@ package telran.multithreading;
 
 public class Printer extends Thread {
 	
-	private char symbol;
-	private int nSymbols;
+//	private char symbol;
+//	private int nSymbols;
+//	
+//	public Printer(char symbol, int nSymbols) {
+//		this.symbol = symbol;
+//		this.nSymbols = nSymbols;
+//	}
+//	@Override
+//	public void run() {
+//		for(int i = 0; i < nSymbols; i++) {
+//			System.out.println(symbol);
+//			try {
+//				sleep(1);
+//			} catch (InterruptedException e) {
+//				
+//			}
+//		}
+//	}
+	private char[] symbols;
+	private boolean running = true;
 	
-	public Printer(char symbol, int nSymbols) {
-		this.symbol = symbol;
-		this.nSymbols = nSymbols;
+	public Printer(String symbols) {
+	this.symbols = symbols.toCharArray();
+	}
+
+	public void stopPrinter() {
+		this.running = false;
 	}
 	@Override
 	public void run() {
-		for(int i = 0; i < nSymbols; i++) {
-			System.out.println(symbol);
+		
+		int index = 0;
+		while(running) {
+			System.out.print(symbols[index]);
 			try {
-				sleep(1);
+				sleep(1000);
 			} catch (InterruptedException e) {
 				
+				index++;
+				if(index == symbols.length) {
+					index = 0;
+				}
 			}
 		}
 	}
