b3c89283d45b37cedbb299b24c564838a357a05c
AlexKovalchuk <kovalchuk111@gmail.com>
2023-09-12 13:59:14 +0300

    HW #45 vers.2 with mutex

--- src/telran/multithreading/Runner.java
+++ src/telran/multithreading/Runner.java
@@ -1,34 +1,37 @@
 package telran.multithreading;
 
-import java.io.PushbackInputStream;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
 public class Runner extends Thread {
-private Race race;
-private int runnerId;
-public Runner(Race race, int runnerId) {
-	this.race = race;
-	this.runnerId = runnerId;
-}
-@Override
-public void run() {
-	int sleepRange = race.getMaxSleep() - race.getMinSleep() + 1;
-	int minSleep = race.getMinSleep();
-	int distance = race.getDistance();
-	Instant time = Instant.now();
-	for (int i = 0; i < distance; i++) {
-		try {
-			sleep((long) (minSleep + Math.random() * sleepRange));
-						
-		} catch (InterruptedException e) {
-			throw new IllegalStateException();
-		}
-		System.out.println(runnerId);
+	private Race race;
+	private int runnerId;
+	private static Object mutex = new Object();
+
+	public Runner(Race race, int runnerId) {
+		this.race = race;
+		this.runnerId = runnerId;
 	}
-	//race.setWinner(runnerId);
-	long timeRace = ChronoUnit.MILLIS.between(time, Instant.now());
-	Race.tableRunners.put(runnerId, (int) timeRace);
-	
-}
+	@Override
+	public void run() {
+		int sleepRange = race.getMaxSleep() - race.getMinSleep() + 1;
+		int minSleep = race.getMinSleep();
+		int distance = race.getDistance();
+		Instant time = Instant.now();
+			for (int i = 0; i < distance; i++) {
+				try {
+					sleep((long) (minSleep + Math.random() * sleepRange));
+						
+				} catch (InterruptedException e) {
+				throw new IllegalStateException();
+				}
+				System.out.println(runnerId);
+			}
+		//race.setWinner(runnerId);
+		long timeRace = ChronoUnit.MILLIS.between(time, Instant.now());
+		
+		synchronized(mutex) {
+			Race.tableRunners.put(runnerId, (int) timeRace);
+		}
+	}
 }
